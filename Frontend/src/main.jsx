796e9ef496b021537230f653440450af9b6a8506
vikrammadhikunta <bhargavi3399@iiitmanipur.ac.in>
2026-04-07 17:11:09 +0530

    fixing Not found

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import Success from "./Success";
+import App from "./App.jsx";
+import Success from "./Success.jsx";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
